f145b10556e03b9c0cd40d5ad28a7fe8cfea899b
ShreyanshEC088 <shreyanshmishragkp2022@gmail.com>
2023-07-05 23:44:43 +0530

    bottom-btn added

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ icons.forEach(icon => {
     icon.classList.remove('shake-icon');
   });
 });
+{/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $('#back-to-top-btn').fadeIn();
+    } else {
+      $('#back-to-top-btn').fadeOut();
+    }
+  });
+
+  $('#back-to-top-btn').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    return false;
+  });
+}); */}
